0d5c5639126848496cfc38525a68c0639276538f
K0mii <madison.o.mayer@gmail.com>
2026-08-08 11:56:08 -0500

    i have no idea what im doi ng but i finally got python to run on  css wetc but now none of my photos r loading omg.

--- script.py
+++ script.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 
-def hello_world():
-    return 'Hello World'
+def home():
+    css = url_for('static',filename = 'style.css')
+    return render_template('index.html', css_path =css)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
